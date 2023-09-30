2011fa601bd41175a2bca207b91ce902472a1981
lurkyphish2085 <lurkyphish2085@users.noreply.github.com>
2023-09-30 21:14:05 +0800

    ClickableTextWithLabel component can now be disabled

--- app/src/main/java/xyz/lurkyphish2085/capstone/palmhiram/ui/components/ClickableTextWithLabel.kt
+++ app/src/main/java/xyz/lurkyphish2085/capstone/palmhiram/ui/components/ClickableTextWithLabel.kt
@@ -2,17 +2,25 @@ package xyz.lurkyphish2085.capstone.palmhiram.ui.components
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import xyz.lurkyphish2085.capstone.palmhiram.ui.theme.PalmHiramTheme
 
 @Composable
@@ -20,25 +28,30 @@ fun ClickableTextWithLabel(
     label: String,
     text: String,
     onClick: (Int) -> Unit,
+    isEnabled: Boolean = true,
     modifier: Modifier = Modifier
 ) {
     Column(
         verticalArrangement = Arrangement.Bottom,
         horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
     ) {
         Text(
             text = label,
             style = MaterialTheme.typography.labelSmall,
         )
+
         ClickableText(
             text = AnnotatedString(
                 text = text,
                 spanStyle = SpanStyle(
                     fontWeight = FontWeight.SemiBold,
-                    color = MaterialTheme.colorScheme.primary,
+                    color = if (isEnabled) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.secondary,
                 )
             ),
-            onClick = onClick
+            onClick = {
+                if (isEnabled) onClick(it)
+            }
         )
     }
 }
@@ -48,11 +61,21 @@ fun ClickableTextWithLabel(
 fun ClickableTextWithLabelPreview() {
     PalmHiramTheme {
         Surface {
-            ClickableTextWithLabel(
-                label = "New here?",
-                text = "Sign up here",
-                onClick = {}
-            )
+            var enable by remember {
+                mutableStateOf(true)
+            }
+
+            Column {
+                ClickableTextWithLabel(
+                    label = "New here?",
+                    text = "Sign up here",
+                    isEnabled = enable,
+                    onClick = {}
+                )
+
+                Spacer(modifier = Modifier.height(32.dp))
+                Button(onClick = { enable = !enable }) { Text(text = if (enable) "Disable" else "Enable") }
+            }
         }
     }
 }
--- app/src/main/java/xyz/lurkyphish2085/capstone/palmhiram/ui/screens/OTPScreen.kt
+++ app/src/main/java/xyz/lurkyphish2085/capstone/palmhiram/ui/screens/OTPScreen.kt
@@ -1,15 +1,26 @@
 package xyz.lurkyphish2085.capstone.palmhiram.ui.screens
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import xyz.lurkyphish2085.capstone.palmhiram.ui.components.ClickableTextWithLabel
 import xyz.lurkyphish2085.capstone.palmhiram.ui.components.ScreenTitleBar
+import xyz.lurkyphish2085.capstone.palmhiram.ui.components.TextFieldWithError
 import xyz.lurkyphish2085.capstone.palmhiram.ui.components.WideButton
 import xyz.lurkyphish2085.capstone.palmhiram.ui.theme.PalmHiramTheme
 
@@ -38,7 +49,29 @@ fun OTPScreen(
 fun OTPInputContent(
     modifier: Modifier = Modifier
 ) {
+    Box(modifier) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .align(Alignment.Center)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {
+            TextFieldWithError(
+                passingCondition = { true },
+                label = "OTP Code"
+            )
 
+            Spacer(modifier = Modifier.heightIn(128.dp))
+
+            ClickableTextWithLabel(
+                label = "Didn't get it?",
+                text = "Resend code",
+                onClick =  {}
+            )
+        }
+    }
 }
 
 @ExperimentalMaterial3Api
