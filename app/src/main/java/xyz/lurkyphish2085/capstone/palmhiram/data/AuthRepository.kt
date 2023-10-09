42d93c8122daeeb877c5c5f840fb713a9bc5ad1c
lurkyphish2085 <lurkyphish2085@users.noreply.github.com>
2023-10-09 17:10:06 +0800

    AuthRepositoryImpl ready

---
+++ app/src/main/java/xyz/lurkyphish2085/capstone/palmhiram/data/AuthRepository.kt
@@ -0,0 +1,10 @@
+package xyz.lurkyphish2085.capstone.palmhiram.data
+
+import com.google.firebase.auth.FirebaseUser
+
+interface AuthRepository {
+    val currentUser: FirebaseUser?
+    suspend fun login(email: String, password: String): Resource<FirebaseUser>
+    suspend fun signup(username: String, email: String, password: String): Resource<FirebaseUser>
+    fun logout()
+}
