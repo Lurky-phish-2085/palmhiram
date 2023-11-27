4abfb0a95d5ef6d133852b0a20a0a3d03cf7a699
lurkyphish2085 <lurkyphish2085@users.noreply.github.com>
2023-11-28 00:53:44 +0800

    added maxkeppeler's sheets-compose-dialogs library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,6 +84,11 @@ dependencies {
 
     implementation("androidx.navigation:navigation-compose:2.7.0")
 
+    // Dialog Library
+    // Implementing the `core` module is mandatory for using other use cases.
+    implementation("com.maxkeppeler.sheets-compose-dialogs:core:1.2.0")
+    implementation("com.maxkeppeler.sheets-compose-dialogs:calendar:1.2.0")
+
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-compiler:2.44")
 
