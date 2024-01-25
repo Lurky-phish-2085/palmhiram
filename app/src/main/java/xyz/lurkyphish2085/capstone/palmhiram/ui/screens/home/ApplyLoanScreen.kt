022103882438669aa290ed12497245e83dad7f29
lurkyphish2085 <Lurky-phish-2085@users.noreply.github.com>
2024-01-25 18:45:12 +0800

    ApplyLoanScreen updated to have a remarks textfield

--- app/src/main/java/xyz/lurkyphish2085/capstone/palmhiram/ui/screens/home/ApplyLoanScreen.kt
+++ app/src/main/java/xyz/lurkyphish2085/capstone/palmhiram/ui/screens/home/ApplyLoanScreen.kt
@@ -12,12 +12,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
@@ -28,7 +34,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,6 +46,7 @@ import xyz.lurkyphish2085.capstone.palmhiram.ui.components.TextFieldWithError
 import xyz.lurkyphish2085.capstone.palmhiram.ui.components.TopBarWithBackButton
 import xyz.lurkyphish2085.capstone.palmhiram.ui.components.WideButton
 import xyz.lurkyphish2085.capstone.palmhiram.ui.theme.PalmHiramTheme
+import xyz.lurkyphish2085.capstone.palmhiram.ui.utils.InputValidationUtils
 import xyz.lurkyphish2085.capstone.palmhiram.utils.Money
 
 @ExperimentalMaterial3Api
@@ -54,6 +63,13 @@ fun ApplyLoanScreen(
     var loanAmount by rememberSaveable {
         mutableStateOf("0.00")
     }
+    var remarks by rememberSaveable {
+        mutableStateOf("")
+    }
+
+    var allFieldsValid by rememberSaveable {
+        mutableStateOf(false)
+    }
 
     Scaffold(
         topBar = {
@@ -64,6 +80,7 @@ fun ApplyLoanScreen(
         },
         bottomBar = {
             WideButton(
+                enabled = allFieldsValid,
                 text = "SUBMIT",
                 onclick = {
                     viewModel?.submitLoanRequest(Money.valueOf(loanAmount))
@@ -74,7 +91,11 @@ fun ApplyLoanScreen(
     ) { padding ->
         ApplyLoanScreenContent(
             submissionFlow = submissionFlow,
-            onAmountChange = { loanAmount = it },
+            onAmountChange = { loanValue, remarksValue, validity ->
+                loanAmount = loanValue
+                remarks = remarksValue
+                allFieldsValid = validity
+            },
             onSubmit = onSubmit,
             modifier = Modifier
                 .padding(padding)
@@ -86,11 +107,37 @@ fun ApplyLoanScreen(
 @Composable
 fun ApplyLoanScreenContent(
     submissionFlow: State<Resource<LoanTransaction>?>?,
-    onAmountChange: (String) -> Unit,
+    onAmountChange: (String, String, Boolean) -> Unit,
     onSubmit: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     val context = LocalContext.current
+
+    var loanAmountField by rememberSaveable {
+        mutableStateOf("0.00")
+    }
+    var remarksField by rememberSaveable {
+        mutableStateOf("")
+    }
+    var remarksFieldValid by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    fun isLoanAmountFieldValid(fieldInput: String): Boolean {
+        return fieldInput.isNotBlank() && InputValidationUtils.validateNumericWithPoints(fieldInput)
+    }
+    fun isRemarksFieldValid(fieldInput: String): Boolean {
+        return fieldInput.length in 1..257
+    }
+    fun updateFieldsValidity(loanAmountValue: String, remarksValue: String): Boolean {
+        return isLoanAmountFieldValid(loanAmountValue) && isRemarksFieldValid(remarksValue)
+    }
+    fun updateFields(loanAmtField: String = loanAmountField, remarksFid: String = remarksField) {
+        loanAmountField = loanAmtField
+        remarksField = remarksFid
+        onAmountChange(loanAmountField, remarksField, updateFieldsValidity(loanAmountField, remarksField))
+    }
+
 
     Box(modifier) {
         submissionFlow?.value?.let {
@@ -125,12 +172,27 @@ fun ApplyLoanScreenContent(
         ) {
             TextFieldWithError(
                 label = "Enter amount",
-                passingCondition = { it.isNotBlank() },
+                passingCondition = { isLoanAmountFieldValid(it) },
                 onValueChange = {
-                    onAmountChange(it)
+                    loanAmountField = it
+                    updateFields(loanAmtField = it)
                 },
                 keyboardType = KeyboardType.Number,
             )
+
+            OutlinedTextField(
+                value = remarksField,
+                onValueChange = {
+                    remarksField = it
+                    updateFields(remarksFid = it)
+                    remarksFieldValid = isRemarksFieldValid(it)
+                },
+                label = { Text("Write Your Remarks Here") },
+                minLines = 12,
+                maxLines = 12,
+            )
+
+            Text("${remarksField.length}/256", color = if (!remarksFieldValid) MaterialTheme.colorScheme.error else MaterialTheme.colorScheme.outline)
         }
     }
 }
